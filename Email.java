738be49969a0e68bddf31c032faa7921e8ec9710
bouaby <ybinsf@yahoo.com>
2017-03-21 20:09:52 -0700

    Add files via upload

---
+++ Email.java
@@ -0,0 +1,368 @@
+/*************************************************************************
+ * Author: Younes Bouab
+ * Email:  YBINSF@YAHOO.COM
+ * Date:   01-01-2017
+ *************************************************************************/
+
+package com.logscanner;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.*;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+
+public class Email {
+	private static String sSMTP, sSMTPPort, sUser, sPassword;
+	private static final String FILE_PATH_SEP = ",";
+	private static final int iMaxFileBytesReadWrite = 1024000; // 1MB
+	private static long iMaxEmailSizeInBytes = -1;
+	private static long iMaxFileSizeInBytes = -1;
+	private static String sBody_Trim_Msg = "Body content trimmed! Maximum Email size reached, ";
+	private static byte[] baBodyTrimMsgBytes = null;
+	private static String sFile_Trim_Msg = "File content was trimmed! Maximum Email size reached, ";
+	private static byte[] baFileTrimMsgBytes = null;
+	private String sPreTrimMsg = "\n\n***";
+	private String sPostTrimMsg = "***";
+	private Session session;
+
+	// Prepare eMail
+	public Email(String sSMTP, String sSMTPPort, String sUser, String sPassword) {
+		this.sSMTP = sSMTP;
+		this.sSMTPPort = sSMTPPort;
+		this.sUser = sUser;
+		this.sPassword = sPassword;
+
+		// SMTP Properties
+		Properties lSTMPProp = new Properties();
+		//lSTMPProp.put("mail.transport.protocol", "smtp");
+		lSTMPProp.put("mail.smtp.host", sSMTP);
+		if (sSMTPPort!=null && !"0".equals(sSMTPPort))
+			lSTMPProp.put("mail.smtp.port", sSMTPPort);
+		
+
+		// Authentication
+		Authenticator auth = null;
+		if (sUser != null && sPassword != null)
+		{
+			lSTMPProp.put("mail.smtp.auth", "true");
+			if (sSMTPPort!=null && !"0".equals(sSMTPPort))
+			lSTMPProp.put("mail.smtp.socketFactory.port", sSMTPPort);
+			lSTMPProp.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			auth = new SMTPAuthenticator();
+		}
+
+		// Session
+		session = Session.getDefaultInstance(lSTMPProp, auth);
+		// session.setDebug(true);
+	}
+
+	public void setAttachmentsProperties(long lMaxEmailSizeInBytes,
+			long lMaxFileSizeInBytes) throws Exception {
+		Email.iMaxEmailSizeInBytes = lMaxEmailSizeInBytes;
+		Email.iMaxFileSizeInBytes = lMaxFileSizeInBytes;
+		sBody_Trim_Msg += lMaxEmailSizeInBytes;
+		baBodyTrimMsgBytes = sBody_Trim_Msg.getBytes("UTF8");
+		sFile_Trim_Msg += lMaxEmailSizeInBytes;
+		baFileTrimMsgBytes = sFile_Trim_Msg.getBytes("UTF8");
+	}
+
+	public void prepForMime(String sMime) throws Exception {
+		if (!Utils.isEmpty(sMime) && sMime.toLowerCase().indexOf("html") != -1) {
+			sPreTrimMsg = "<br><br><b>***";
+			sPostTrimMsg = "***</b>";
+		}
+		sBody_Trim_Msg = sPreTrimMsg + sBody_Trim_Msg + iMaxEmailSizeInBytes
+				+ sPostTrimMsg;
+		baBodyTrimMsgBytes = sBody_Trim_Msg.getBytes("UTF8");
+		sFile_Trim_Msg = sPreTrimMsg + sFile_Trim_Msg + iMaxEmailSizeInBytes
+				+ sPostTrimMsg;
+		baFileTrimMsgBytes = sFile_Trim_Msg.getBytes("UTF8");
+	}
+
+	public long getiMaxFilesSizeInBytes() {
+		return iMaxFileSizeInBytes;
+	}
+
+	public int getiMaxFileBytesReadWrite() {
+		return iMaxFileBytesReadWrite;
+	}
+
+	public InternetAddress[] setIAArray(String address) throws Exception {
+		InternetAddress[] iAddressArray = null;
+		if (address != null) {
+			StringTokenizer lSTAddress = new StringTokenizer(address, ",");
+			int numOfIA = lSTAddress.countTokens();
+			iAddressArray = new InternetAddress[numOfIA];
+			int i = 0;
+			while (lSTAddress.hasMoreTokens()) {
+				iAddressArray[i] = new InternetAddress(lSTAddress.nextToken());
+				i++;
+			}
+		}
+		return iAddressArray;
+	}
+
+	// Send eMail
+	public void send(String sMime, String sTo, String sBcc, String sCc,
+			String sFrom, String sSubject, String sBody, String sFiles,
+			String sOutputDir) throws Exception {
+
+		prepForMime(sMime);
+
+		MimeMessage message = new MimeMessage(session);
+		Multipart multipart = new MimeMultipart();
+
+		// Set Email Recipients and Subject
+		InternetAddress iaFromRecipients = new InternetAddress(sFrom);
+		message.setFrom(iaFromRecipients);
+
+		message.setRecipients(Message.RecipientType.TO, setIAArray(sTo));
+		if (sBcc != null)
+			message.setRecipients(Message.RecipientType.BCC, setIAArray(sBcc));
+
+		if (sCc != null)
+			message.setRecipients(Message.RecipientType.CC, setIAArray(sCc));
+
+		message.setSubject(sSubject);
+
+		// Body: Check Size!
+		long iBodySize = 0;
+		if (!Utils.isEmpty(sBody)) {
+			byte[] baBody = sBody.getBytes("UTF8");
+			iBodySize = baBody.length;
+			if (iBodySize > iMaxEmailSizeInBytes) {
+				int iTrimMsgLength = (int) iMaxEmailSizeInBytes;
+				if (baBodyTrimMsgBytes != null)
+					iTrimMsgLength -= baBodyTrimMsgBytes.length;
+				byte[] bnewBody = new byte[iTrimMsgLength];
+				System.arraycopy(baBody, 0, bnewBody, 0, iTrimMsgLength);
+				sBody = new String(bnewBody) + sBody_Trim_Msg;
+				iBodySize = sBody.getBytes("UTF8").length;
+			}
+		}
+
+		// Attach Body
+		MimeBodyPart mbpMessage = new MimeBodyPart();
+		mbpMessage.setContent(sBody, sMime);
+		multipart.addBodyPart(mbpMessage);
+		sBody = null;
+
+		if (iBodySize < iMaxEmailSizeInBytes) {
+			// Files
+			if (!Utils.isEmpty(sFiles))
+				processAttachments(multipart, sMime, sFiles, iBodySize);
+		}
+
+		// Put Message
+		message.setContent(multipart);
+
+		// Send eMail!
+		try {
+			Transport.send(message);
+		} catch (SendFailedException aSFE) {
+			// Get the valid and invalid email addresses
+			String sStatusMsg = "Checking if email sent was becuase of an invalid adress";
+			Utils.log(sStatusMsg);
+			Address[] aValid = aSFE.getValidUnsentAddresses();
+			Address[] aInvalid = aSFE.getInvalidAddresses();
+			String sInvalidList = "";
+			String sValidList = "";
+			if (aInvalid != null && aInvalid.length > 0) {
+
+				for (int i = 0; i < aInvalid.length; i++)
+					sInvalidList += aInvalid[i].toString() + ",";
+				sInvalidList = sInvalidList.substring(0,
+						sInvalidList.length() - 1);
+
+				for (int i = 0; i < aValid.length; i++)
+					sValidList += aValid[i].toString() + ",";
+				sValidList = sValidList.substring(0, sInvalidList.length() - 1);
+
+				sStatusMsg += "Could NOT send this email to the following addresses \""
+						+ sInvalidList + "\"";
+				Utils.log(sStatusMsg);
+
+				sStatusMsg += "Attempting to send this email to the valid addresses only \""
+						+ sValidList + "\"";
+				Utils.log(sStatusMsg);
+
+				send(sMime, sValidList, sCc, sBcc, sFrom, sSubject, sBody,
+						sFiles, sOutputDir);
+				sStatusMsg += "Email sent successfully!";
+				Utils.log(sStatusMsg);
+			}
+			throw (aSFE);
+		} finally {
+			multipart = null;
+			message = null;
+		}
+	}
+
+	// Process Attachments
+	public static String processAttachments(Multipart multipart, String sMime,
+			String sFiles, long lBodySize) throws Exception {
+
+		// Loop Through files
+		String result = null;
+		String[] saFiles = sFiles.split(FILE_PATH_SEP);
+
+		// Get Total Size of email (body + attached files) and remove any excess
+		// files!
+		File file = null;
+		long lTotalfilesSize = lBodySize;
+		int saFilesLength = saFiles.length;
+		int maxFilesIndex = 0;
+		for (int i = 0; i < saFilesLength; i++) {
+			String sFilePartPath = saFiles[i];
+			file = new File(sFilePartPath);
+			long fileSize = file.length();
+			if (lTotalfilesSize + fileSize <= iMaxEmailSizeInBytes) {
+				maxFilesIndex++;
+				lTotalfilesSize += fileSize;
+			} else {
+				long lDiff = iMaxEmailSizeInBytes - lTotalfilesSize;
+				if (lDiff > 0) {
+					String validFilePartName = splitFile(sFilePartPath, lDiff,
+							1);
+					saFiles[i] = validFilePartName;
+					lTotalfilesSize = iMaxEmailSizeInBytes;
+					maxFilesIndex++;
+				}
+				break;
+			}
+		}
+
+		// Split/Attach Files per max File Size/Email
+		for (int i = 0; i < maxFilesIndex; i++) {
+			String sFilePartPath = saFiles[i];
+			file = new File(sFilePartPath);
+			long lFileSize = file.length();
+
+			String fileParts = null;
+			if (iMaxFileSizeInBytes != -1 && lFileSize > iMaxFileSizeInBytes)
+				fileParts = splitFile(sFilePartPath, iMaxFileSizeInBytes, -1);
+			if (fileParts != null) {
+				String[] saValidFiles = fileParts.split(FILE_PATH_SEP);
+				int saValidFilesLength = saValidFiles.length;
+				for (int j = 0; j < saValidFilesLength; j++)
+					attachValidFile(saValidFiles[j], multipart, sMime);
+			} else
+				attachValidFile(sFilePartPath, multipart, sMime);
+			file = null;
+		}
+		saFiles = null;
+		return result;
+	}
+
+	public static void attachValidFile(String filePath, Multipart multipart,
+			String sMime) throws MessagingException {
+		MimeBodyPart fileMimeBodyPart = new MimeBodyPart();
+		FileDataSource lFDS = new FileDataSource(filePath);
+		DataHandler dh = new DataHandler(lFDS);
+		fileMimeBodyPart.setDataHandler(dh);
+		fileMimeBodyPart.setFileName(lFDS.getName());
+		multipart.addBodyPart(fileMimeBodyPart);
+		fileMimeBodyPart = null;
+		lFDS = null;
+		dh = null;
+	}
+
+	public static String splitFile(String sFileName, long iMaxFileSizeInBytes,
+			long iMaxParts) throws IOException {
+		String result = null;
+		RandomAccessFile raf = new RandomAccessFile(sFileName, "r");
+		long sourceFileSizeInBytes = raf.length();
+
+		// Trim File Message
+		if (sourceFileSizeInBytes > iMaxFileSizeInBytes) {
+			// File Parts
+			long numParts = iMaxParts;
+			if (numParts == -1)
+				numParts = sourceFileSizeInBytes / iMaxFileSizeInBytes;
+			long remainingPartBytes = sourceFileSizeInBytes
+					% iMaxFileSizeInBytes;
+			BufferedOutputStream bos = null;
+			String sPartFileName = null;
+
+			// Read/Write Chunks of a File Part
+			long numReadWriteParts = iMaxFileSizeInBytes
+					/ iMaxFileBytesReadWrite;
+			long remainingReadWriteBytes = iMaxFileSizeInBytes
+					% iMaxFileBytesReadWrite;
+
+			// Read/Write File Parts
+			for (int i = 1; i <= numParts; i++) {
+				// write chunks of the Part File at a time
+				sPartFileName = sFileName + "_Part" + i;
+				bos = new BufferedOutputStream(new FileOutputStream(
+						sPartFileName));
+
+				for (int j = 0; j < numReadWriteParts; j++)
+					readWrite(raf, bos, iMaxFileBytesReadWrite, sFileName);
+
+				if (remainingReadWriteBytes > 0)
+					readWrite(raf, bos, remainingReadWriteBytes, sFileName);
+
+				// Flush Part File if not last one
+				bos.close();
+				bos = null;
+
+				// Return File Parts
+				if (result == null)
+					result = sPartFileName;
+				else
+					result += FILE_PATH_SEP + sPartFileName;
+			}
+
+			if (remainingPartBytes > 0
+					&& (iMaxParts == -1 || (numParts + 1) <= iMaxParts)) {
+				sPartFileName = sFileName + "_Part" + (numParts + 1);
+				bos = new BufferedOutputStream(new FileOutputStream(
+						sPartFileName));
+				readWrite(raf, bos, remainingPartBytes, sFileName);
+				readWrite(bos, baFileTrimMsgBytes);
+				bos.close();
+				bos = null;
+				result += "," + sPartFileName;
+			}
+
+			// Cleanup
+			raf.close();
+			raf = null;
+		}
+		return result;
+	}
+
+	private static void readWrite(BufferedOutputStream bos,
+			byte[] baTrimMsgBytes) throws IOException {
+		bos.write(baTrimMsgBytes);
+		bos.flush();
+	}
+
+	public static void readWrite(RandomAccessFile raf,
+			BufferedOutputStream bos, long lBufSize, String sRafName)
+			throws IOException {
+		byte[] buf = new byte[(int) lBufSize];
+		if (raf.read(buf) != -1) {
+			bos.write(buf);
+			bos.flush();
+		} else
+			throw (new IOException(
+					"Was not able to read from RandomAccessFile " + sRafName));
+	}
+
+	private class SMTPAuthenticator extends javax.mail.Authenticator {
+		public PasswordAuthentication getPasswordAuthentication() {
+			String username = sUser;
+			String password = sPassword;
+			return new PasswordAuthentication(username, password);
+		}
+	}
+}
---
+++ ScanThread.java
@@ -0,0 +1,211 @@
+package com.logscanner;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Date;
+
+public class ScanThread extends Thread {
+	private int iThreadId = -1;
+	private LogScanner logScanner = null;
+	private String sRregExp = null;
+
+	protected ScanThread(int iThreadId, String sRregExp, String key,
+			LogScanner logScanner) {
+		this.iThreadId = iThreadId;
+		this.sRregExp = sRregExp;
+		this.setName(key);
+		this.logScanner = logScanner;
+	}
+
+	public void run() {
+		Props props = null;
+		FileInfo fileInfo = null;
+		String sFilePath = this.getName();
+		boolean bDateParsed = false;
+		boolean bPassedDateFilter = false;
+		double dFileSize = 0L;
+		String sFileSize = null;
+		String sFileDateFormat = null;
+		int iFileDateStart = -1;
+		String sStatusMsg = null;
+		int iFileLinesPassedCount = 0;
+		int iFileLinesCounter = 0;
+		Date dStartDate = null;
+		Date dEndDate = null;
+		FileReader frFileReader = null;
+		BufferedReader brBufferReader = null;
+		StringBuffer sbReportContent = null;
+
+		try {
+			// ///////////////////////////////////////////////
+			// Start Thread
+			// ///////////////////////////////////////////////
+			sbReportContent = new StringBuffer("");
+			props = logScanner.getProps();
+			if (sRregExp == null)
+				fileInfo = props.getMapFileInfo().get(this.getName());
+			else
+				fileInfo = props.getMapFileInfo().get(sRregExp);
+			dStartDate = new Date();
+			sFileDateFormat = fileInfo.getDateFormat();
+			iFileDateStart = fileInfo.getDateStartPos();
+			dFileSize = fileInfo.getSize();
+			sFileSize = Utils.formatFileBytesSize(dFileSize);
+			sStatusMsg = "Scan Thread #" + iThreadId + " \"" + this.getName()
+					+ "\" started";
+			Utils.log(sStatusMsg);
+			LogScanner.formatFileTitle(dStartDate, sStatusMsg, sbReportContent,
+					props);
+			File file = new File(sFilePath);
+			frFileReader = new FileReader(file);
+			brBufferReader = new BufferedReader(frFileReader);
+
+			// Filter Tests
+			boolean bPassedFilter = false;
+			boolean bPreviousPassedFilter = false;
+			String sFileLine = null;
+			String sEncodedFileLine = null;
+			String sPreviousFileLine = null;
+			String sPreviousEncodedFileLine = null;
+
+			// Read & test file line by line
+			while ((sFileLine = brBufferReader.readLine()) != null) {
+				// Initialize
+				bPassedFilter = false;
+				bPassedDateFilter = false;
+
+				if (!Utils.isEmpty(sFileLine)) {
+					// Check if line similar to previous line
+					if (sFileLine.equals(sPreviousFileLine)) {
+						bPassedFilter = bPreviousPassedFilter;
+						sEncodedFileLine = sPreviousEncodedFileLine;
+					}
+					// Run Filter Tests
+					else {
+						// 0 Parse Date
+						String sFileLineDate = Utils.getDateFromLine(sFileLine,
+								sFileDateFormat, iFileDateStart);
+
+						// 1 HTML Encode
+						sEncodedFileLine = Utils.encodeHTML(sFileLine, props);
+
+						if (!Utils.isEmpty(sFileLineDate)) {
+							bDateParsed = true;
+
+							// Check End Date
+							bPassedFilter = Utils.compareEndDate(
+									props.getFilterEndDate(),
+									Utils.sSTD_DATE_FORMAT, sFileLineDate,
+									sFileDateFormat);
+
+							// End File Scan if did not pass End Date
+							if (!bPassedFilter) {
+								sStatusMsg = "Scan Thread #" + iThreadId
+										+ " stopped at line #"
+										+ iFileLinesCounter + "!  Line Date \""
+										+ sFileLineDate
+										+ "\" is after Filter End Date \""
+										+ props.getFilterEndDate() + "\"";
+								LogScanner.formatFileSummary(
+										iFileLinesPassedCount, dFileSize,
+										dStartDate, dEndDate, sStatusMsg,
+										sbReportContent, props);
+
+							}
+
+							// Check Start Date
+							bPassedFilter = Utils.compareStartDate(
+									sFileLineDate, sFileDateFormat,
+									props.getFilterStartDate(),
+									Utils.sSTD_DATE_FORMAT);
+
+							if (bPassedFilter)
+								bPassedDateFilter = true;
+						} else
+							// Previous line must have passed Filter
+							bPassedFilter = bPreviousPassedFilter;
+
+						if (bPassedFilter) {
+							// 4 Not Filter
+							sEncodedFileLine = Utils.checkLineForWords(
+									sEncodedFileLine, "not", props);
+							if (sEncodedFileLine != null) {
+								bPassedFilter = true;
+								// 5 And Filter
+								sEncodedFileLine = Utils.checkLineForWords(
+										sEncodedFileLine, "and", props);
+								if (sEncodedFileLine != null) {
+									bPassedFilter = true;
+									// 6 Or Words Filter
+									sEncodedFileLine = Utils.checkLineForWords(
+											sEncodedFileLine, "or", props);
+									if (sEncodedFileLine != null)
+										bPassedFilter = true;
+									else
+										bPassedFilter = false;
+								} else
+									bPassedFilter = false;
+							} else
+								bPassedFilter = false;
+						}
+					}
+				}
+
+				// ///////////////////////////////////
+				// 7 Cumulative Filter Test
+				// ///////////////////////////////////
+				if (bPassedFilter) {
+					sbReportContent.append(iFileLinesCounter + ": "
+							+ sEncodedFileLine
+							+ props.getFileFormat().getLineBreak());
+					iFileLinesPassedCount++;
+				}
+
+				// Set Previous Filter Test Results
+				sPreviousFileLine = sFileLine;
+				sPreviousEncodedFileLine = sEncodedFileLine;
+				bPreviousPassedFilter = bPassedFilter;
+				iFileLinesCounter++;
+			}
+
+			// ///////////////////////////////////////////////
+			// Append Scan Statuses to Report and Log
+			// ///////////////////////////////////////////////
+			dEndDate = new Date();
+			if (!bDateParsed)
+				sbReportContent
+						.append("Not a single date was parsed successfuly from file!"
+								+ props.getFileFormat().getLineBreak());
+			else if (!bPassedDateFilter)
+				sbReportContent.append("Date parsed, but none passed Filter!"
+						+ props.getFileFormat().getLineBreak());
+			sStatusMsg = "Scan Thread #" + iThreadId + " ended";
+			LogScanner.formatFileSummary(iFileLinesPassedCount, dFileSize,
+					dStartDate, dEndDate, sStatusMsg, sbReportContent, props);
+
+		} catch (Exception e) {
+			sStatusMsg = "Scaning Thread #" + iThreadId + " \"";
+			Utils.handleException(new Date(), e, sStatusMsg, sbReportContent,
+					props);
+		}
+
+		finally {
+
+			// Add Scan File Report to main Report File
+			logScanner.appendToSBMainReportContent(sbReportContent);
+
+			// ///////////////////////////////////////////////
+			// Cleanup
+			// ///////////////////////////////////////////////
+			sFileDateFormat = null;
+			sStatusMsg = null;
+			sbReportContent = null;
+			dStartDate = null;
+			dEndDate = null;
+			frFileReader = null;
+			brBufferReader = null;
+			sFilePath = null;
+		}
+	}
+}
